8e1077fa72e83b9e119e4a1072837809836f8c60
DerevenetsArtyom <fenix7036@gmail.com>
2016-09-14 23:15:30 +0300

    Add api url for REST

--- tango_project/urls.py
+++ tango_project/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'^rango/', include('rango.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
