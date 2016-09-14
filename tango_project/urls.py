8cb0ee11c0607b79f6801537ab20a122a020efb9
DerevenetsArtyom <fenix7036@gmail.com>
2016-09-14 23:31:57 +0300

    Add routers for pages and categories

--- tango_project/urls.py
+++ tango_project/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from registration.backends.simple.views import RegistrationView
 
+from rest_framework import routers
+
+from rango import views
+
+router = routers.DefaultRouter()
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'pages', views.PageViewSet)
+
 
 # Create a new class that redirects the user to the index page, if successful at logging
 class MyRegistrationView(RegistrationView):
@@ -15,5 +23,7 @@ urlpatterns = [
     url(r'^rango/', include('rango.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
